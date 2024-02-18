75d3a97d9194e79b0bfcea0a10ec5ec8c67e6f93
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-02-18 09:31:15 +0530

    created video db and singleton instance of db

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,15 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 // TODO (01) Create a @Dao interface called VideoDao.
 @Dao
@@ -33,4 +37,23 @@ interface VideoDao {
     // TODO (03) Add SQL @Insert insertAll() that replaces on conflict (or upsert).
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+// check whether INSTANCE is initialized, and, if it isn’t, use DatabaseBuilder to create it.
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
 }
